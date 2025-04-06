c98bc12ed7cc44352b7759fc67e7ca53e7788ce0
xRafay <123715905+xRafay@users.noreply.github.com>
2025-04-06 23:00:30 +0500

    Create test_app.py

---
+++ .circleci/test_app.py
@@ -0,0 +1,12 @@
+import unittest
+from app import add, subtract
+
+class TestMath(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 2), 3)
+
+if __name__ == '__main__':
+    unittest.main()
